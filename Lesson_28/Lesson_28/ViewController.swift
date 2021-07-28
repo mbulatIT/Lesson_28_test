6657f3af5d996950fffd69fe85b0c1170285f50f
mbulatIT <41010055+mbulatIT@users.noreply.github.com>
2021-07-28 20:59:29 +0300

    Implement feature 2

--- Lesson_28/Lesson_28/ViewController.swift
+++ Lesson_28/Lesson_28/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var feature2Button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func feature2ButtonPressed(_ sender: Any) {
+        print("feature 2")
+    }
+    
 }
 
